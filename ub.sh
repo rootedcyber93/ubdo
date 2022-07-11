dd91c74f56ca2221737750a4aa00b21d5a34b434
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-07-11 13:23:37 +0530

    Add files via upload

---
+++ ub.sh
@@ -0,0 +1,4 @@
+rm -rf Ult*;git clone https://github.com/rootedcyber93/ubdo;cd ubdo
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t ubdo
+docker run --privileged --env-file .env --rm -i ubdo
+ls
