bdca9a87ffe5dad08ab364b9a9abeaf59ad77a38
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-07-11 13:39:23 +0530

    Update ub.sh

--- ub.sh
+++ ub.sh
@@ -1,4 +1,7 @@
-git clone https://github.com/rootedcyber93/ubdo;cd ubdo
+cd ubdo
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ubdo
 docker run --privileged --env-file .env --rm -i ubdo
 ls
+cd Ult*
+pwd
+ls /
