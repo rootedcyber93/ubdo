4b39a4d69f4da0453235507d3f357958d88b0011
rootedcyber93 <108932489+rootedcyber93@users.noreply.github.com>
2022-07-20 21:45:49 +0530

    Update ubs.sh

--- ubs.sh
+++ ubs.sh
@@ -1,6 +1,6 @@
 clear
-git clone https://github.com/rooted-cyber/Ultroid
-#git clone -b rootedcyber-dev https://github.com/rooted-cyber/Ultroid
+#git clone https://github.com/rooted-cyber/Ultroid
+git clone -b dev https://github.com/TeamUltroid/Ultroid
 cd Ultr*;pip3 install --no-cache-dir -r requirements.txt;pip3 install -r res*/st*/opt*
 cd Ult*
 wget -O startup https://gist.githubusercontent.com/rooted-cyber/dfdc5c46bd00e6b1c03b041ca9568616/raw/dbd1b23abc33b8d354972526d0fdbdea7b34050c/st
